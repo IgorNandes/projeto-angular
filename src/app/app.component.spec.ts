0a2c9fbb827465378abd4547f9a81e7f4e377541
Igor Nandes <igornandes@hotmail.com>
2023-03-09 00:38:02 -0300

    master push

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projeto-angular'`, () => {
+  it(`should have as title 'projeto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-angular');
+    expect(app.title).toEqual('projeto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto app is running!');
   });
 });
